6311066114bdc615fc47bcfdb776216eaade33e1
Diana Morocho <mp.dianalexa@gmail.com>
2014-04-09 14:53:26 -0500

    MODELO DE CLASES PARA LA APP PRODUCTO CLASES{Categoria, AreaProduccion, Operacion, Producto}

--- waiter/settings.py
+++ waiter/settings.py
@@ -31,11 +31,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'waiter.apps.producto',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -62,6 +64,7 @@ DATABASES = {
         'USER': 'waiter',
         'PASSWORD':'waiter',
         'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
