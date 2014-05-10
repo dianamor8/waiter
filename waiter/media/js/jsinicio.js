ba097e2105ebabad90f99758dc2603ed4d10e7cd
dianamor8 <mp.dianalexa@gmail.com>
2014-05-09 23:58:15 -0500

    drop model panel. carousel of the divs

--- waiter/media/js/jsinicio.js
+++ waiter/media/js/jsinicio.js
@@ -37,42 +37,6 @@ $(document).ready(function() {
 		var posi = $(link).offset().top;
 		$('body,html').animate({scrollTop:posi},700);
 	});
-
-
-	/* copy loaded thumbnails into carousel */
-	$('.panel .img-responsive').on('load', function(){
-
-	}).each(function(i) {
-		if(this.complete) {
-			var item = $('<div class="item"></div>');
-			var itemDiv = $(this).parent('a');
-			var title = $(this).parent('a').attr("title");
-			item.attr("title",title);
-			$(itemDiv.html()).appendTo(item);
-			item.appendTo('#modalCarousel .carousel-inner'); 
-			if (i==0){ // set first item active
-				item.addClass('active');
-			}
-		}
-	});
-
-	/* activate the carousel */
-	$('#modalCarousel').carousel({interval:false});
-
-	/* change modal title when slide changes */
-	$('#modalCarousel').on('slid.bs.carousel', function () {
-		$('.modal-title').html($(this).find('.active').attr("title"));
-	})
-
-	/* when clicking a thumbnail */
-	$('.panel-thumbnail>a').click(function(e){
-		e.preventDefault();
-		var idx = $(this).parents('.panel').parent().index();
-		var id = parseInt(idx);
-		$('#myModal').modal('show'); // show the modal
-		$('#modalCarousel').carousel(id); // slide carousel to selected
-		return false;
-	});
 });
 
 
